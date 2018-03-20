5849323f76aa1ee2f0d726ee21a508a80f946dd4
Rafael Acevedo <raa7@cin.ufpe.br>
2018-03-20 16:43:37 -0300

    feat(metrics): add service latency metrics

---
+++ src/injector/middleware.go
@@ -0,0 +1,23 @@
+package injector
+
+import (
+	"github.com/inloco/kafka-elasticsearch-injector/src/metrics"
+	"github.com/inloco/kafka-elasticsearch-injector/src/models"
+	"time"
+)
+
+type instrumentingMiddleware struct {
+	metricsPublisher metrics.MetricsPublisher
+	next             Service
+}
+
+func (s instrumentingMiddleware) Insert(records []*models.Record) error {
+	begin := time.Now()
+	err := s.next.Insert(records)
+	s.metricsPublisher.RecordEndpointLatency(time.Since(begin).Seconds())
+	return err
+}
+
+func (s instrumentingMiddleware) ReadinessCheck() bool {
+	return s.next.ReadinessCheck()
+}

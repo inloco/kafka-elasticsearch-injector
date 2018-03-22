1d57f8055635dffcc584c7a59b5a861c9c40bbd9
Mateus Moury <mateus@inlocomedia.com>
2018-03-22 16:23:09 -0300

    feat(elastic_codecs): adding codecs that transforms a kafka record into a record to be inserted in elasticsearch

---
+++ src/elasticsearch/codec_test.go
@@ -0,0 +1,79 @@
+package elasticsearch
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/inloco/kafka-elasticsearch-injector/src/kafka/fixtures"
+	"github.com/inloco/kafka-elasticsearch-injector/src/logger_builder"
+	"github.com/inloco/kafka-elasticsearch-injector/src/models"
+)
+
+var codecLogger = logger_builder.NewLogger("elasticsearch-test")
+
+func TestCodec_EncodeElasticRecords(t *testing.T) {
+	codec := &basicCodec{
+		config: Config{},
+		logger: codecLogger,
+	}
+	record, id, value := fixtures.NewRecord(time.Now())
+
+	elasticRecords, err := codec.EncodeElasticRecords([]*models.Record{record})
+	if assert.NoError(t, err) && assert.Len(t, elasticRecords, 1) {
+		elasticRecord := elasticRecords[0]
+		assert.Equal(t, fmt.Sprintf("%s-%s", record.Topic, record.FormatTimestamp()), elasticRecord.Index)
+		assert.Equal(t, record.Topic, elasticRecord.Type)
+		assert.Equal(t, fmt.Sprintf("%d:%d", record.Partition, record.Offset), elasticRecord.ID)
+		assert.Equal(t, id, elasticRecord.Json["id"])
+		assert.Equal(t, value, elasticRecord.Json["value"])
+	}
+}
+
+func TestCodec_EncodeElasticRecords_ColumnsBlacklist(t *testing.T) {
+	codec := &basicCodec{
+		config: Config{BlacklistedColumns: []string{"value"}},
+		logger: codecLogger,
+	}
+	record, _, _ := fixtures.NewRecord(time.Now())
+
+	elasticRecords, err := codec.EncodeElasticRecords([]*models.Record{record})
+	if assert.NoError(t, err) && assert.Len(t, elasticRecords, 1) {
+		elasticRecord := elasticRecords[0]
+		assert.Contains(t, elasticRecord.Json, "id")
+		assert.NotContains(t, elasticRecord.Json, "value")
+	}
+}
+
+func TestCodec_EncodeElasticRecords_IndexColumn(t *testing.T) {
+	indexPrefix := "prefix"
+
+	codec := &basicCodec{
+		config: Config{Index: indexPrefix, IndexColumn: "id"},
+		logger: codecLogger,
+	}
+	record, id, _ := fixtures.NewRecord(time.Now())
+
+	elasticRecords, err := codec.EncodeElasticRecords([]*models.Record{record})
+	if assert.NoError(t, err) && assert.Len(t, elasticRecords, 1) {
+		elasticRecord := elasticRecords[0]
+		assert.Equal(t, fmt.Sprintf("%v-%v", indexPrefix, id), elasticRecord.Index)
+	}
+}
+
+func TestCodec_EncodeElasticRecords_DocIDColumn(t *testing.T) {
+	codec := &basicCodec{
+		config: Config{DocIDColumn: "id"},
+		logger: codecLogger,
+	}
+	record, id, _ := fixtures.NewRecord(time.Now())
+
+	elasticRecords, err := codec.EncodeElasticRecords([]*models.Record{record})
+	if assert.NoError(t, err) && assert.Len(t, elasticRecords, 1) {
+		elasticRecord := elasticRecords[0]
+		assert.Equal(t, strconv.Itoa(int(id)), elasticRecord.ID)
+	}
+}

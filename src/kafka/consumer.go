fb59f68eb60c31c731f59796ca59a714f4f4b1f8
Luiz Nogueira <luiz.nogueira@inloco.com.br>
2020-03-10 15:16:21 -0300

    debug

--- src/kafka/consumer.go
+++ src/kafka/consumer.go
@@ -150,6 +150,7 @@ func (k *kafka) worker(consumer *cluster.Consumer, buffSize int, notifications c
 						)
 						continue
 					}
+					level.Debug(k.consumer.Logger).Log("message decoded", req)
 					decoded = append(decoded, req)
 				}
 			}
